811a115ef77688ed04e79ec4599d12bd5b4966e5
Felipe <lamediatricolor@gmail.com>
2025-12-28 11:15:42 -0500

    Complete Intro to CS: Insertion python and test individual script

--- Path00 - Computer Science/Course01 - Complete Intro to CS/python/sort_algorithms.py
+++ Path00 - Computer Science/Course01 - Complete Intro to CS/python/sort_algorithms.py
@@ -1,5 +1,3 @@
-import pytest
-
 def bubble_sort(array: list):
     n = len(array)
     cambios = True
@@ -14,22 +12,28 @@ def bubble_sort(array: list):
     
     return array
 
-@pytest.mark.parametrize("entrada, esperado", [
-    ([19, 25, 0, 5, 3, 8, 6, 2, 1, -1, 10, -2, -6, -5, 13, -100], [-100, -6, -5, -2, -1, 0, 1, 2, 3, 5, 6, 8, 10, 13, 19, 25]),
-    
-    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
-    
-    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
-    
-    ([3, 1, 2, 3, 1], [1, 1, 2, 3, 3]),
-    
-    ([], []),
 
-    ([1], [1]),
-    
-    ([7, 7, 7], [7, 7, 7])
-])
-def test_bubble_sort_varios_casos(entrada, esperado):
+def insertion_sort(array: list):
+    n = len(array)
 
-    resultado = bubble_sort(entrada.copy())
-    assert resultado == esperado
+    for i in range(1, n):
+        print(array)
+        num = array[i]  # Almaceno el numero actual de la lista no-ordenada
+        j = i - 1       # Almaceno posicion final de la lista ordenada
+        
+        # Recorrer la lista ordenada de derecha a izquierda, comparando con el numero actual
+        # Condicion 1: j debe ser mayor o igual a 0, cuidando indices negativos
+        # Condicion 2: El numero del bucle interno debe ser mayor al numero actual para que este vaya "bajando"
+        while j >= 0 and array[j] > num:
+            array[j + 1] = array[j]
+            j -= 1
+            print(array)
+        
+        # Cuando llegamos al final de la lista (numero menor que todos)
+        # o encontramos un numero menor al numero actual, actualizamos la posicion del numero
+        array[j + 1] = num
+
+        print()
+    
+    return array
+
---
+++ Path00 - Computer Science/Course01 - Complete Intro to CS/python/test.py
@@ -0,0 +1,26 @@
+import pytest
+from sort_algorithms import *
+
+@pytest.mark.parametrize("entrada, esperado", [
+    ([19, 25, 0, 5, 3, 8, 6, 2, 1, -1, 10, -2, -6, -5, 13, -100], [-100, -6, -5, -2, -1, 0, 1, 2, 3, 5, 6, 8, 10, 13, 19, 25]),
+    
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+    
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
+    
+    ([3, 1, 2, 3, 1], [1, 1, 2, 3, 3]),
+    
+    ([], []),
+
+    ([1], [1]),
+    
+    ([7, 7, 7], [7, 7, 7])
+])
+
+def test_bubble_sort_varios_casos(entrada, esperado):
+
+    #resultado = bubble_sort(entrada.copy())
+    
+    resultado = insertion_sort(entrada.copy())
+
+    assert resultado == esperado
